b0c1330f4e40c85a7042851025b65a0570bd8405
hshy1839 <hongjeongmin1839@gmail.com>
2024-06-11 12:06:19 +0900

    헤더 메뉴 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,13 +30,27 @@ const Header = () => {
           <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
           <path className="line" d="M7 16 27 16"></path>
         </svg><div className="menu">
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/qa">Q&A</Link>
-      </div>
+          <Link to="/">Home</Link>
+          <Link to="">
+            <div className='menu-contact-container'>Contact
+              <div className='menu-contact'>
+                <button className='menu-instagram-btn'> <a href="https://www.instagram.com/seokhyung98/" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram style={{ color: '#DD2A7B', fontSize: '20px', marginRight: '10px' }} />
+                </a>Instagram</button>
+                <button className='menu-mail-btn'><a href="mailto:hongjeongmin1839@gmail.com">
+                  <FaEnvelope style={{ color: 'skyblue', fontSize: '20px ', marginRight: '10px' }} />
+                </a>Mail</button>
+                <button className='menu-mail-btn'><a href="https://github.com/hshy1839/hshy1839">
+                  <FaGithub style={{ color: 'white', fontSize: '20px ', marginRight: '10px' }} />
+                </a>Github</button>
+              </div>
+            </div>
+          </Link>
+          <Link to="/qa">Q&A</Link>
+        </div>
       </label>
 
-    </header>
+    </header >
   );
 };
 
