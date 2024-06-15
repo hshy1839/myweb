2f40808016068a47ebab5c544d38846b344b3368
hshy1839 <hongjeongmin1839@gmail.com>
2024-06-16 01:31:18 +0800

    header 수정완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Header.css';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  // State 업데이트 시 애니메이션 효과 주기
+  useEffect(() => {
+    const menuElement = document.querySelector('.menu');
+
+    if (menuOpen) {
+      // 메뉴 열기
+      menuElement.style.height = '500px'; // 원하는 높이 설정
+      menuElement.style.opacity = '1'; // 투명도 설정
+    } else {
+      // 메뉴 닫기
+      menuElement.style.height = '0'; // 높이 초기화
+      menuElement.style.opacity = '0'; // 투명도 초기화
+    }
+  }, [menuOpen]);
 
   const handleMouseEnter = () => {
     setMenuOpen(true);
@@ -31,25 +46,29 @@ const Header = () => {
         </Link>
       </div>
       <div className="btn-container">
-        <button className='header-button'>about</button>
-        <button className='header-button'>contact</button>
-        <button className='header-button'>news</button>
-        {menuOpen && (
-          <div className="menu" onMouseEnter={handleMouseEnter} >
-            <div className='menu-item-container'>
-              <span className='menu-item-title'>About</span>
-              <Link to="" className="menu-item">누구냐 넌</Link>
-              <Link to="" className="menu-item">Our Team</Link>
-              <Link to="" className="menu-item">Contact Us</Link>
-            </div>
-            <div className='menu-item-container'>
-              <span className='menu-item-title'>Contact</span>
-              <Link to="" className="menu-item">Support</Link>
-              <Link to="" className="menu-item">Latest News</Link>
-              <Link to="" className="menu-item">Events</Link>
-            </div>
+        <button className='header-button'>About</button>
+        <button className='header-button'>Contact</button>
+        <button className='header-button'>News</button>
+        <div className={`menu ${menuOpen ? 'open' : ''}`}>
+          <div className='menu-item-container'>
+            <span className='menu-item-title'>About</span>
+            <Link to="" className="menu-item">개발자 이야기</Link>
+            <Link to="" className="menu-item">도전의 역사</Link>
+            <Link to="" className="menu-item">Contact Us</Link>
           </div>
-        )}
+          <div className='menu-item-container'>
+            <span className='menu-item-title'>Contact</span>
+            <Link to="https://github.com/hshy1839" target="_blank" className="menu-item">Github</Link>
+            <Link to="https://www.instagram.com/seokhyung98/" target="_blank" className="menu-item">Instagram</Link>
+            <Link to="mailto:hongjeongmin1839@google.com" target="_blank" className="menu-item">Mail</Link>
+          </div>
+          <div className='menu-item-container'>
+            <span className='menu-item-title'>News</span>
+            <Link to="" className="menu-item">공지사항</Link>
+            <Link to="" className="menu-item">알림</Link>
+            <Link to="" className="menu-item">게시판</Link>
+          </div>
+        </div>
       </div>
     </header>
   );
